2634994d5f0cbd34c9f55fead319b02ee0fe3917
tiffahny <tiff9490@gmail.com>
2018-08-08 17:25:03 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
 // Write your code in this file!
 
+function getUKTime() {
+  var result=new Date().getHours() + 5
+  return result
+}
+ 
+getUKTime()
+
+function discountTicket(price){
+  var result= 0.8 * price
+  return result
+}
+
+function tweetReply(fanName){
+  var result= "Hey " + fanName +", thanks so much for the support! I really appreciate it!"
+  return result
+}
 
